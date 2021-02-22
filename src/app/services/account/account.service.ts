0fdbd2ce9317e07e69179d3c78635915a603cd15
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-02-22 19:25:58 +0100

    Created a simple username and password retriever function

--- src/app/services/account/account.service.ts
+++ src/app/services/account/account.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { CookieOptionsProvider, CookieService, COOKIE_OPTIONS } from 'ngx-cookie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  constructor() { }
+
+  private static key_username : string = "com.codecool.cookies.username"
+  private static key_password : string = "com.codecool.cookies.password"
+
+  constructor(private cookieService : CookieService) {
+
+  }
+
+  getUsername() : string {
+    return this.cookieService.get(AccountService.key_username);
+  }
+
+  getPassword(): string{
+    return this.cookieService.get(AccountService.key_password);
+  }
+
+  setUsername(username : string) : void{
+    this.cookieService.put(AccountService.key_username,username, )
+  }
+
 }
