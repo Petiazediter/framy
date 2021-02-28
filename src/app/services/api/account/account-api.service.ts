b75036497a9bce445442a16bc2859544410f11b1
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-02-28 12:49:17 +0100

    Created an insert account function to account-service.

--- src/app/services/api/account/account-api.service.ts
+++ src/app/services/api/account/account-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Account } from 'src/app/model/Account';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,12 @@ export class AccountApiService {
 
   getAllAccount() : Observable<Account[]>{
     return this.httpClient.get<Account[]>(`${this.baseUrl}${this.accountsApi}`)
+  }
+
+  addAccount(account : Account) : Observable<Account>{
+    const headers = {'content-type':'application/json'}
+    const body = JSON.stringify(account);
+    return this.httpClient.post<Account>(`${this.baseUrl}${this.accountsApi}`, body, {'headers':headers})
   }
   
 }
