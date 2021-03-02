283758be137d3ef87e96e73c31924c7f05ce4735
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-03-02 12:02:42 +0100

    Data validation before sending post method.

--- src/app/components/logincomponent/register/register/register.component.ts
+++ src/app/components/logincomponent/register/register/register.component.ts
@@ -8,14 +8,31 @@ import { NgForm } from '@angular/forms'
 })
 export class RegisterComponent implements OnInit {
 
+  isLoading : boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.isLoading = false
+    console.log(this.isLoading)
   }
 
   onSubmit(form : NgForm ){
     console.log(form)
+    // Add account to 
+    this.isLoading =true
+    this.accountService.addAccount(this.account).subscribe(
+      (response) => {                           //Next callback
+        console.log('response received ' + response)
+        this.isLoading = false
+      },
+      (error) => {                              //Error callback
+        console.error('error caught in component ' + error)
+        alert("Server error. Try again later.")
+        this.isLoading = false
+    });
+    
+    return true
   }
 
 }
