f89c65eee8d17631bc8d99568f4fe0918542542c
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-03-02 10:02:25 +0100

    Added login css to the register page to display the same designed form

--- src/app/components/logincomponent/register/register/register.component.ts
+++ src/app/components/logincomponent/register/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['../../login/login/login.component.scss','../../../../app.component.css']
 })
 export class RegisterComponent implements OnInit {
 
