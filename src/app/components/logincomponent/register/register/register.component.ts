3e08f75210051373d35c63ec84a3fac71a75778b
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-03-02 10:33:04 +0100

    Added onSubmit method to the component

--- src/app/components/logincomponent/register/register/register.component.ts
+++ src/app/components/logincomponent/register/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form : NgForm ){
+    console.log(form)
   }
 
 }
